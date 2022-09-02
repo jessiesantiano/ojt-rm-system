27ba4f29e4ae1b656c7130b340fb3d4a97bc0a89
PaulPintang <pauljpintang@gmail.com>
2022-09-02 15:36:33 +0800

    fix: redirect to dashboard if currently logged in

--- admin/index.php
+++ admin/index.php
@@ -1,3 +1,10 @@
+<?php
+    session_start();
+    if(isset($_SESSION['id'])&&!empty($_SESSION['id'])){
+      header("Location: ./pages/dashboard/");
+      exit();
+    }
+?>
 <!DOCTYPE html>
 <html lang="en">
   <head>
