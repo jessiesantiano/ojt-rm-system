c4468e637084c6b52bb2291b220bba95970d52d1
PaulPintang <pauljpintang@gmail.com>
2022-09-21 14:05:28 +0800

    student profile: remove y overflow

--- admin/pages/students/profile.php
+++ admin/pages/students/profile.php
@@ -140,7 +140,7 @@ while ($row = mysqli_fetch_array($students)) {
 
   <!-- Document Section -->
 
-  <div class="flex flex-wrap -mx-3">
+  <div class="flex flex-wrap">
     <div class="w-full max-w-full px-3 mt-6 md:w-6/12 md:flex-none">
       <div class="relative flex flex-col min-w-0 break-words bg-white border-0 shadow-soft-xl rounded-2xl bg-clip-border">
         <div class="p-6 px-4 pb-0 mb-0 bg-white border-b-0 rounded-t-2xl">
@@ -207,7 +207,7 @@ while ($row = mysqli_fetch_array($students)) {
   <!-- Document Section End -->
 
   <!-- Report Section -->
-  <div class="flex flex-wrap -mx-3 py-3">
+  <div class="flex flex-wrap py-3">
     <div class="flex-none w-full max-w-full px-3">
       <div class="relative flex flex-col min-w-0 mb-6 break-words bg-white border-0 border-transparent border-solid shadow-soft-xl rounded-2xl bg-clip-border">
         <div class="p-6 pb-0 mb-0 bg-white border-b-0 border-b-solid rounded-t-2xl border-b-transparent">
