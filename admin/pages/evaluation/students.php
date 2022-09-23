0d9a37e29da138fe077ef9f2fb21fe1e066cbd36
PaulPintang <pauljpintang@gmail.com>
2022-09-23 15:59:14 +0800

    fix: getting request bug

--- admin/pages/evaluation/students.php
+++ admin/pages/evaluation/students.php
@@ -1,7 +1,8 @@
 <?php
     // connection
     include "../../../connection.php";
-     $results = mysqli_query($db, "SELECT * FROM students WHERE Swcompany = '$accountFor' AND iSmidterm = 'requested' OR iSfinal = 'requested'");
+     $results = mysqli_query($db, "SELECT * FROM students WHERE Swcompany = '$accountFor' AND iSmidterm = 'requested' AND iSfinal IS NULL OR iSfinal = 'requested'");
+
 ?>
 	<!--Container-->
 					<div class="w-full md:w-4/5 xl:w-3/5" style="padding: 0 40px 0 40px">
