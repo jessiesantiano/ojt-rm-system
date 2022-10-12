a990a3f9e717fdaab4a5766904eaed4073760940
PaulPintang <pauljpintang@gmail.com>
2022-10-12 15:44:13 +0800

    UI: company/school dashboard

--- admin/pages/dashboard/dashboard.php
+++ admin/pages/dashboard/dashboard.php
@@ -162,9 +162,89 @@
                 
           <!-- <div class="flex gap-2 px-3 items-center w-full py-5 mb-10" style="height: calc(100vh - 360px)"> -->
           <div class="flex gap-2 px-3 items-center w-full py-5">
-            <div style="width: 90%;">
-              <canvas id="myChart"></canvas>
-            </div>
+              <?php if ($accountFor == 'Coordinator') : ?>
+                <div style="width: 90%;">
+                  <canvas id="myChart"></canvas>
+                </div>
+              <?php else : ?>
+                <div class="bg-white rounded-lg pt-10 md:pt-0 lg:pt-0 w-full h-full  px-2 md:px-0 lg:px-0">
+                    <div class="py-3 w-full">
+                      <h5 class="mb-3 text-gray-800 w-full" style="line-height: 18px"><span class="text-pink-400 font-bold pr-1">Generate </span>Profile Report</h5>
+                        <table class="min-w-full divide-y divide-gray-200 border-collapse w-full">
+                          <thead class="sticky top-0">
+                              <tr>
+                                  <th scope="col" class="px-3 md:px-6 lg:px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider bg-white shadow-sm">
+                                    Profile
+                                  </th>
+                                  <th scope="col" class="px-3 md:px-6 lg:px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider bg-white shadow-sm">
+                                    Course
+                                  </th>
+                                  <th scope="col" class="px-3 md:px-6 lg:px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider bg-white shadow-sm">
+                                    Midterm
+                                  </th>
+                                  <th scope="col" class="px-3 md:px-6 lg:px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider bg-white shadow-sm">
+                                    Final
+                                  </th>
+                                  <th scope="col" class="md:hidden lg:hidden px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider bg-white shadow-sm">
+                                  </th>
+                              </tr>
+                          </thead>
+                        <?php $results = mysqli_query($db, "SELECT * FROM students WHERE courseCode = '$courseCode' OR Swcompany = '$accountFor'"); ?>
+                        <?php while ($row = mysqli_fetch_array($results)) { ?>
+
+                            <tbody class=" divide-y divide-gray-200 overflow-auto text-gray-500">
+                                <tr>
+                                    <td class="px-3 md:px-6 lg:px-6 py-2 whitespace-nowrap">
+                                        <div class="flex items-center">
+                                            <div class=" flex items-center gap-2">
+                                                <div class="flex-shrink-0 h-10 w-10">
+                                                    <?php if ($row['Sphoto'] == NULL) : ?>
+                                                        <img class="h-10 w-10 rounded-full" src="temp-profile.jpg" alt="">
+                                                    <?php else : ?>
+                                                      <img src="../../../student/image/' . <?php echo $row['Sphoto']; ?>" alt="" class="h-10 w-10 rounded-full"/>
+                                                    <?php endif; ?>
+                                                </div>
+                                                <small><?php echo $row['Sname']; ?> <?php echo $row['Slname']; ?></small>
+                                            </div>
+                                        </div>
+                                    </td>
+                                    <td class="px-3 md:px-6 lg:px-6 py-2 whitespace-nowrap">
+                                        <?php echo $row['Scourse']; ?>
+                                    </td>
+                                    <td class="px-3 md:px-6 lg:px-6 py-2 whitespace-nowrap">
+                                        <?php if ($row['iSmidterm'] == 'graded') : ?>
+                                            <span class="bg-green-100 text-green-800 text-xs font-semibold mr-2 px-2.5 py-0.5 rounded dark:bg-green-200 dark:text-green-900">Graded</span>
+                                         <?php else : ?>
+                                          <span class="bg-yellow-100 text-yellow-800 text-xs font-semibold mr-2 px-2.5 py-0.5 rounded dark:bg-yellow-200 dark:text-yellow-900">Pending</span>
+                                        <?php endif; ?>
+                                    </td>
+                                    <td class="px-3 md:px-6 lg:px-6 py-2 whitespace-nowrap">
+                                        <?php if ($row['iSfinal'] == 'graded') : ?>
+                                            <span class="bg-green-100 text-green-800 text-xs font-semibold mr-2 px-2.5 py-0.5 rounded dark:bg-green-200 dark:text-green-900">Graded</span>
+                                         <?php else : ?>
+                                          <span class="bg-yellow-100 text-yellow-800 text-xs font-semibold mr-2 px-2.5 py-0.5 rounded dark:bg-yellow-200 dark:text-yellow-900">Pending</span>
+                                        <?php endif; ?>
+                                    </td>
+                                    <td class="px-3 md:px-6 lg:px-6 py-2 whitespace-nowrap">
+                                    <form action="../students/student-profilepdf.php" method="GET" target="_blank">
+                                     <input type="hidden" name="id" value="<?php echo $row['id']; ?>">
+                                     <input type="hidden" name="studentID" value="<?php echo $row['studentID']; ?>">  
+                                     <button type="submit" name="generate_id" class="ml-2  px-2 py-1 font-bold text-center text-white uppercase align-middle transition-all rounded-lg cursor-pointer bg-gradient-to-tl from-purple-700 to-pink-500 leading-pro text-xs ease-soft-in tracking-tight-soft shadow-soft-md bg-150 bg-x-25 hover:scale-102 active:opacity-85 hover:shadow-soft-xs">
+                                        <i class="fa fa-folder-open" aria-hidden="true"></i>&nbsp; Generate
+                                     </button>
+                                    </form>
+
+                                    </td>
+                                </tr>
+                            </tbody>
+                          <?php } ?>
+
+                        </table>
+                    </div>   
+                    
+              </div>
+              <?php endif; ?>
+            
             <div class="bg-white rounded-lg flex items-center justify-center pt-10 md:pt-0 lg:pt-0 w-2/4 h-full  px-5 md:px-0 lg:px-0">
                 <div class="flex flex-col h-full w-full font-light">
                     <div class="h-full w-full bg-pink-400 rounded-t flex justify-center items-center ">
@@ -180,8 +260,6 @@
                 </div>
               </div>
            </div>
-           
-
         </div>
       </div>
 
@@ -194,25 +272,19 @@
             $ojt[] = $row["school"];
             $trainees[] = $row['trainees'];
           }
-        }elseif ($courseCode == 'Economics Dept.') {
+        }
+        if ($courseCode == 'Economics Dept.') {
           $results = mysqli_query($db, "SELECT * FROM companies");
           while ($row = mysqli_fetch_array($results)) {
             $ojt[] = $row["company"];
             $trainees[] = $row['trainees'];
           }
-        }else{
-          echo "other layout";
         }
-       
-
-
-        // chart data
-       
 
       ?>
+
       <script>
         const labels = <?php echo json_encode($ojt) ?>;
-
         const data = {
           labels: labels,
           datasets: [{
@@ -239,23 +311,25 @@
             data: <?php echo json_encode($trainees) ?>,
           }]
         };
+          const config = {
+            type: 'bar',
+            data: data,
+            options: {
+              scales: {
+                y: {
+                  beginAtZero: true
+                }
+              }
+            },
+          };
+          const myChart = new Chart(
+            document.getElementById('myChart'),
+            config
+          );
 
-      const config = {
-        type: 'bar',
-        data: data,
-        options: {
-          scales: {
-            y: {
-              beginAtZero: true
-            }
-          }
-        },
-      };
-        const myChart = new Chart(
-          document.getElementById('myChart'),
-          config
-        );
       </script>
+
+     
       <script src="calendar/js/jquery.min.js"></script>
       <script src="calendar/js/main.js"></script>
     <?php
