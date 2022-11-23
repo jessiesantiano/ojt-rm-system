7114cd008e2b28b3544e8d0664f834e44097272d
judemichael03 <judemichaelraborar.dolom@bicol-u.edu.ph>
2022-11-23 10:38:45 +0800

    changes

--- admin/pages/students/profile.php
+++ admin/pages/students/profile.php
@@ -220,7 +220,7 @@ while ($row = mysqli_fetch_array($students)) {
         <div class="flex-none w-full max-w-full px-3">
           <div class="relative flex flex-col min-w-0 mb-6 break-words bg-white border-0 border-transparent border-solid shadow-soft-xl rounded-2xl bg-clip-border">
             <div class="p-6 pb-0 mb-0 bg-white border-b-0 border-b-solid rounded-t-2xl border-b-transparent">
-              <h6>List of Documents After OJT</h6>
+              <h6>Certificate</h6>
             </div>
             <div class="flex-auto px-0 pt-0 pb-2">
               <div class="p-0 overflow-x-auto">
